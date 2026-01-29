831fe6b9ab628d671b1a6f479f8f7d5718b8834d
kaustubh7s <kaustubhshejwal7@gmail.com>
2026-01-29 21:59:30 +0530

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,4 +1,19 @@
-<?php include '../config.php'; ?>
+<?php
+include '../config.php';
+
+if(isset($_POST['login'])){
+    $u=$_POST['user'];
+    $p=sha1($_POST['pass']);
+
+    $q=mysqli_query($conn,"SELECT * FROM admin WHERE username='$u' AND password='$p'");
+    if(mysqli_num_rows($q)){
+        $_SESSION['admin']=$u;
+        header("Location: dashboard.php");
+    } else {
+        $error="Invalid login";
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -6,35 +21,20 @@
 <title>Admin Login</title>
 <style>
 body{background:#fff0f5;font-family:Arial}
-.login{width:300px;margin:100px auto;background:#fff;padding:20px;border-radius:10px}
+.box{width:320px;margin:120px auto;background:#fff;padding:25px;border-radius:12px}
 button{background:#ff69b4;border:none;color:#fff;padding:10px;width:100%}
 </style>
 </head>
 <body>
 
-<div class="login">
+<div class="box">
 <h2>Admin Login</h2>
-
+<?php if(isset($error)) echo $error; ?>
 <form method="post">
-<input type="text" name="user" placeholder="Username" required><br><br>
+<input name="user" placeholder="Username" required><br><br>
 <input type="password" name="pass" placeholder="Password" required><br><br>
 <button name="login">Login</button>
 </form>
-
-<?php
-if(isset($_POST['login'])){
-    $u=$_POST['user'];
-    $p=$_POST['pass'];
-
-    $q=mysqli_query($conn,"SELECT * FROM admin WHERE username='$u' AND password='$p'");
-    if(mysqli_num_rows($q)){
-        $_SESSION['admin']=$u;
-        header("Location: dashboard.php");
-    } else {
-        echo "Invalid login";
-    }
-}
-?>
 </div>
 
 </body>
